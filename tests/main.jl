f572c47725a240450926cc871910d2b9eb2ae4f9
Changhyun Kwon <chkwon@gmail.com>
2024-04-02 19:43:26 +0900

    gif generation

--- tests/main.jl
+++ tests/main.jl
@@ -2,11 +2,11 @@
 import Random,Plots
 using Concorde, Test
 using Random, Base.Math, Plots
-include("../src/generate_dist.jl")
+include("../src/functions.jl")
 include("../src/implementations.jl")
 include("../src/strategies.jl")
 
-n_nodes = 5
+n_nodes = 30
 # (dist/1000) 해줘야 함!
 X, Y, dist = generate_distance_matrix(n_nodes)
 opt_tour, opt_len = solve_tsp(X, Y; dist="EUC_2D")
@@ -34,9 +34,11 @@ opt_gap_heuristic = (heuristic_tour_len - opt_len) / opt_len
 
 
 # show the images
-count = 0
-for tour_edges in total_tour_edges
-    count +=1
-    generate_and_save_plots(X, Y, tour_edges, "first_strategy_$count.png")
+let
+    count = 0
+    anim = @animate for tour_edges in total_tour_edges
+        count +=1
+        generate_and_save_plots(X, Y, tour_edges, "png/first_strategy_$count.png")
+    end
+    gif(anim, "tsp.gif", fps = 1)
 end
-
